e719d43c413b439b3807d7dc653c6bad074f255d
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-30 16:25:42 -0300

    chore: change jest.config to js

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
