f605e56a3fefe37cf6c3bb7d8857e0d0be4d16b1
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-30 20:25:36 -0300

    feat: ensure AddMessageController returns 400 if message is not provided

--- src/presentation/controllers/message/add-message/add-message-controller.spec.ts
+++ src/presentation/controllers/message/add-message/add-message-controller.spec.ts
@@ -2,13 +2,6 @@ import { MissingParamError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 import faker from 'faker'
 import { AddMessageController } from './add-message-controller'
-import { HttpRequest } from './add-message-controller-protocols'
-
-const mockRequest = (): HttpRequest => ({
-  body: {
-    message: faker.lorem.paragraphs(3)
-  }
-})
 
 type SutTypes = {
   sut: AddMessageController
@@ -24,7 +17,23 @@ const makeSut = (): SutTypes => {
 describe('Add Message Controller', () => {
   it('Should return 400 if name is not provided', async () => {
     const { sut } = makeSut()
-    const httpResponse = await sut.handle(mockRequest())
+    const httpRequest = {
+      body: {
+        message: faker.lorem.paragraphs(3)
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
+  })
+
+  it('Should return 400 if message is not provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: faker.name.findName()
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('message')))
   })
 })
--- src/presentation/controllers/message/add-message/add-message-controller.ts
+++ src/presentation/controllers/message/add-message/add-message-controller.ts
@@ -4,9 +4,12 @@ import { Controller, HttpRequest, HttpResponse } from './add-message-controller-
 
 export class AddMessageController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name } = httpRequest.body
+    const { name, message } = httpRequest.body
     if (!name) {
       return badRequest(new MissingParamError('name'))
+    }
+    if (!message) {
+      return badRequest(new MissingParamError('message'))
     }
     return null
   }
