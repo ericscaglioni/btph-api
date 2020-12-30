63bc42ebf941de1d3098afc5a12f4339fcebbf27
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-30 15:20:33 -0300

    chore: create package json with api informations

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
