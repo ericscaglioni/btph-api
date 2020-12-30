48386de103f7e1f501078938648517249e191923
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-30 17:49:15 -0300

    refactor: change mock to use faker

--- src/domain/test/mock-message.ts
+++ src/domain/test/mock-message.ts
@@ -1,6 +1,10 @@
 import { AddMessageParams } from '@/domain/usecases/message/add-message'
+import faker from 'faker'
+
+const name = faker.name.findName()
+const message = faker.lorem.paragraphs(3)
 
 export const mockAddMessageParams = (): AddMessageParams => ({
-  name: 'any_name',
-  message: 'any_message'
+  name,
+  message
 })
