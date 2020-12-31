84a42d0d9e282917469a2a9deef3e7cd63cde335
Eric Ferreira <eric.ferreira@luizalabs.com>
2020-12-31 10:46:13 -0300

    feat: add middlewares

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
+import { bodyParser, contentType, cors } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
+  app.use(contentType)
 }
