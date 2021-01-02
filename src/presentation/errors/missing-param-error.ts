0f6ffd2401d2ad5b5b5089ec1296a595637938cb
Eric Ferreira <eric.ferreira@luizalabs.com>
2021-01-02 12:46:17 -0300

    fix: adjust name of MissingParamError

--- src/presentation/errors/missing-param-error.ts
+++ src/presentation/errors/missing-param-error.ts
@@ -1,6 +1,6 @@
 export class MissingParamError extends Error {
   constructor (paramName: string) {
-    super('MissingParamError')
-    this.name = `Missing param: ${paramName}`
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
