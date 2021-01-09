e28cbbc952be2b9a25b36117208574ce8401cc9d
Eric Ferreira <eric.ferreira@luizalabs.com>
2021-01-09 11:59:47 -0300

    feat: ensure AuthMiddleware calls LoadUserByToken with correct access token

--- src/presentation/middlewares/auth/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -1,14 +1,27 @@
+import { LoadUserByToken } from '@/domain/usecases/user/load-user-by-token'
 import { unauthorized } from '@/presentation/helpers/http/http-helper'
+import { HttpRequest } from '@/presentation/protocols'
+import { mockLoadUserByToken } from '@/presentation/test'
 import { AuthMiddleware } from './auth-middleware'
+import faker from 'faker'
+
+const mockRequest = (): HttpRequest => ({
+  headers: {
+    'x-access-token': faker.random.uuid()
+  }
+})
 
 type SutTypes = {
   sut: AuthMiddleware
+  loadUserByTokenStub: LoadUserByToken
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware()
+  const loadUserByTokenStub = mockLoadUserByToken()
+  const sut = new AuthMiddleware(loadUserByTokenStub)
   return {
-    sut
+    sut,
+    loadUserByTokenStub
   }
 }
 
@@ -17,5 +30,13 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(unauthorized())
+  })
+
+  it('Should call LoadUserByToken with correct accessToken', async () => {
+    const { sut, loadUserByTokenStub } = makeSut()
+    const loadByTokenSpy = jest.spyOn(loadUserByTokenStub, 'loadByToken')
+    const httpRequest = mockRequest()
+    await sut.handle(httpRequest)
+    expect(loadByTokenSpy).toHaveBeenCalledWith(httpRequest.headers['x-access-token'])
   })
 })
--- src/presentation/middlewares/auth/auth-middleware.ts
+++ src/presentation/middlewares/auth/auth-middleware.ts
@@ -1,11 +1,15 @@
+import { LoadUserByToken } from '@/domain/usecases/user/load-user-by-token'
 import { unauthorized } from '@/presentation/helpers/http/http-helper'
 import { HttpRequest, HttpResponse, Middleware } from '@/presentation/protocols'
 
 export class AuthMiddleware implements Middleware {
+  constructor (private readonly loadUserByToken: LoadUserByToken) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['x-access-token']
-    if (!accessToken) {
-      return unauthorized()
+    if (accessToken) {
+      await this.loadUserByToken.loadByToken(accessToken)
     }
+    return unauthorized()
   }
 }
