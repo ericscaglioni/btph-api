2cb613ea76448ad1ed30186f323f052f1170ccfb
Eric Ferreira <eric.ferreira@luizalabs.com>
2021-01-16 13:05:47 -0300

    refactor: move code to a factory method

--- src/data/usecases/user/add-user/db-add-user.spec.ts
+++ src/data/usecases/user/add-user/db-add-user.spec.ts
@@ -1,8 +1,15 @@
 import { LoadUserByEmailRepository } from '@/data/protocols/db/user'
 import { mockUser } from '@/domain/test'
+import { AddUserParams } from '@/domain/usecases/user/add-user'
 import faker from 'faker'
 import { UserModel } from '../load-user-by-token/db-load-user-by-token-protocols'
 import { DbAddUser } from './db-add-user'
+
+const mockAddUserParams = (): AddUserParams => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
 
 const mockLoadUserByEmailRepository = (): LoadUserByEmailRepository => {
   class LoadUserByEmailRepositortyStub implements LoadUserByEmailRepository {
@@ -31,11 +38,7 @@ describe('Add User usecase', () => {
   it('Should call LoadUserByEmailRepository with correct email', async () => {
     const { sut, loadUserByEmailRepositoryStub } = makeSut()
     const loadByEmailSpy = jest.spyOn(loadUserByEmailRepositoryStub, 'loadByEmail')
-    const params = {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    }
+    const params = mockAddUserParams()
     await sut.add(params)
     expect(loadByEmailSpy).toHaveBeenCalledWith(params.email)
   })
