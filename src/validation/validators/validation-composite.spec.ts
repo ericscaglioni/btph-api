c1395e047dd60ad94fdb71ba3f5d0baa53fd5b97
Eric Ferreira <eric.ferreira@luizalabs.com>
2021-01-17 14:08:24 -0300

    feat: ensure ValidationComposite not return if validation succeeds

--- src/validation/validators/validation-composite.spec.ts
+++ src/validation/validators/validation-composite.spec.ts
@@ -32,4 +32,10 @@ describe('Validation Composite', () => {
     const error = sut.validate({ field: 'any_field' })
     expect(error).toEqual(new Error())
   })
+
+  test('Should not return if validation succeeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
 })
