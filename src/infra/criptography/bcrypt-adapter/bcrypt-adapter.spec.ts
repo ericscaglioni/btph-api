ffa3451575bbcafdf03a233fb1691ddf80d3b9be
Eric Ferreira <eric.ferreira@luizalabs.com>
2021-01-18 20:13:47 -0300

    feat: finish BcryptAdapter

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -19,5 +19,25 @@ describe('Bcrypt Adapter', () => {
       await sut.compare('any_value', 'any_hash')
       expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
     })
+
+    it('Should return true when compare succeeds', async () => {
+      const sut = makeSut()
+      const isValid = await sut.compare('any_value', 'any_hash')
+      expect(isValid).toBeTruthy()
+    })
+
+    it('Should return false when compare fails', async () => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockResolvedValueOnce(false)
+      const isValid = await sut.compare('any_value', 'any_hash')
+      expect(isValid).toBeFalsy()
+    })
+
+    it('Should throw if compare throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockRejectedValueOnce(new Error())
+      const promise = sut.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
